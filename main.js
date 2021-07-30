9953ff26f8a51dfa8cfa6336cdba01fc396fb94d
jonathan-whitehat <76190639+jonathan-whitehat@users.noreply.github.com>
2021-07-29 22:22:27 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+song = "";
+
+function preload()
+{
+	song = loadSound("music.mp3");
+}
+
+function setup() 
+{
+	canvas =  createCanvas(600, 500);
+	canvas.center();
+
+	video = createCapture(VIDEO);
+	video.hide();
+}
+
+function modelLoaded()
+{
+	console.log('PoseNet is Intialized')
+}
+
+function draw() 
+{
+	image(video, 0, 0, 600, 500);
+}
+
+function play()
+{
+	song.play();
+}
